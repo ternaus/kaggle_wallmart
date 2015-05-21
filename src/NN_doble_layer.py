8815cb9648a27448eccadbb00279fc00b2abac3d
Vladimir Iglovikov <iglovikov@gmail.com>
2015-05-21 00:35:13 -0700

    Double modified

--- src/NN_doble_layer.py
+++ src/NN_doble_layer.py
@@ -25,6 +25,65 @@ import xgboost as xgb
 import numpy as np
 import math
 
+
+def float32(k):
+    return np.cast['float32'](k)
+
+class EarlyStopping(object):
+    def __init__(self, patience=100):
+        self.patience = patience
+        self.best_valid = np.inf
+        self.best_valid_epoch = 0
+        self.best_weights = None
+
+    def __call__(self, nn, train_history):
+        current_valid = train_history[-1]['valid_loss']
+        current_epoch = train_history[-1]['epoch']
+        if current_valid < self.best_valid:
+            self.best_valid = current_valid
+            self.best_valid_epoch = current_epoch
+            self.best_weights = [w.get_value() for w in nn.get_all_params()]
+        elif self.best_valid_epoch + self.patience < current_epoch:
+            print("Early stopping.")
+            print("Best valid loss was {:.6f} at epoch {}.".format(
+                self.best_valid, self.best_valid_epoch))
+            nn.load_weights_from(self.best_weights)
+            raise StopIteration()
+
+class AdaptiveVariable(object):
+    def __init__(self, name, start=0.03, stop=0.000001, inc=1.1, dec=0.5):
+        self.name = name
+        self.start, self.stop = start, stop
+        self.inc, self.dec = inc, dec
+
+    def __call__(self, nn, train_history):
+        current_valid = train_history[-1]['valid_loss']
+        if len(train_history) > 1:
+            previous_valid = train_history[-2]['valid_loss']
+        else:
+            previous_valid = np.inf
+        current_value = getattr(nn, self.name).get_value()
+        if current_value < self.stop:
+            raise StopIteration()
+        if current_valid > previous_valid:
+            getattr(nn, self.name).set_value(float32(current_value*self.dec))
+        else:
+            getattr(nn, self.name).set_value(float32(current_value*self.inc))
+
+class AdjustVariable(object):
+    def __init__(self, name, start=0.03, stop=0.001):
+        self.name = name
+        self.start, self.stop = start, stop
+        self.ls = None
+
+    def __call__(self, nn, train_history):
+        if self.ls is None:
+            self.ls = np.linspace(self.start, self.stop, nn.max_epochs)
+
+        epoch = train_history[-1]['epoch']
+        new_value = float32(self.ls[epoch - 1])
+        getattr(nn, self.name).set_value(new_value)
+
 def merge_data(df):
     return ''.join([str(df["store_nbr"]), "_", str(df["item_nbr"]), "_", df["date"]])
 
@@ -130,9 +189,9 @@ net1 = NeuralNet(
         ],
     # layer parameters:
     input_shape=(None, num_features),  # 96x96 input pixels per batch
-    hidden0_num_units=50,  # number of units in hidden layer
+    hidden0_num_units=200,  # number of units in hidden layer
     dropout_p=0.5,
-    hidden1_num_units=50,  # number of units in hidden layer
+    hidden1_num_units=200,  # number of units in hidden layer
     output_nonlinearity=None,  # output layer uses identity function
     output_num_units=1,  # 1 target values
 
@@ -141,9 +200,10 @@ net1 = NeuralNet(
     update_learning_rate=0.0001,
     update_momentum=0.9,
     eval_size=0.2,
-    max_epochs=50,  # we want to train this many epochs
+    max_epochs=200,  # we want to train this many epochs
     verbose=1,
     regression=True,  # flag to indicate we're dealing with regression problem
+
     )
 
 target = (ly-ym) / ys
@@ -174,10 +234,10 @@ grid()
 legend()
 xlabel("epoch")
 ylabel("loss")
-# ylim(1e-3, 1e-2)
-yscale("log")
+ylim(0.5, 1)
+# yscale("log")
 savefig('logs/double.eps', bbox_inches='tight')
-ylim(ymax=1, tmin=0)
+ylim(ymax=1, ymin=0)
 
 fName = open('logs/double.log', 'w')
 print >> fName, 'train mean = ', ym
